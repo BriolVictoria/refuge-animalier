2d7562a8057e9f422788e8a9b6bd22f2cb405334
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-19 16:50:38 +0100

    Add component for the navigation links

--- resources/views/components/public/navigation/navigation_links.blade.php
+++ resources/views/components/public/navigation/navigation_links.blade.php
@@ -1,8 +1,30 @@
+@php
+    $items = [
+       ['list_class'=>'mb-[2.625rem]', 'label'=> 'Accueil', 'title' => 'Vers la page Accueil', 'route_name' => '#', 'links_class' => 'text-lg font-medium mt-[4.5rem] inline-block'],
+       ['list_class'=>'mb-[2.625rem]', 'label'=> 'À propos', 'title' => 'Vers la page À propos', 'route_name' => '#', 'links_class' => 'text-lg font-medium'],
+       ['list_class'=>'mb-[2.625rem]', 'label'=> 'Nos animaux', 'title' => 'Vers la page Nos animaux', 'route_name' => '#', 'links_class' => 'text-lg font-medium'],
+       ['list_class'=>'mb-[2.625rem]', 'label'=> 'Devenir bénévole', 'title' => 'Vers la page Devenir bénévole', 'route_name' => '#', 'links_class' => 'text-lg font-medium'],
+       ['list_class'=>'mb-[2.625rem]', 'label'=> 'Contactez-nous', 'title' => 'Vers la page Contactez-nous', 'route_name' => '#', 'links_class' => 'rounded-[0.25rem] inline-block border-blue-900 border-[0.09375rem] text-sm font-medium pr-[1.875rem] pl-[1.875rem] pt-[0.5rem] pb-[0.5rem] mb-[7.625rem] text-blue-900'],
+
+   ];
+@endphp
+
 <ul class="nav_container flex flex-col items-center">
-    <li class="links_bg">Accueil</li>
-    <li class="links_bg">À propos</li>
-    <li class="links_bg">Nos animaux</li>
-    <li class="links_bg">Devenir bénévole</li>
-    <li class="link_bg">Contactez-nous</li>
-    <img src="{!! asset("assets/img/logo_bg.svg") !!}" alt="Logo les patte sheuresue">
+
+    @foreach($items as $item)
+
+        <x-public.navigation.navigation_link
+            :list_class="$item['list_class']"
+            :label="$item['label']"
+            :title="$item['title']"
+            :links_class="$item['links_class']"/>
+
+    @endforeach
+
+    <a href="{!! route('public.homepage') !!}" title="Vers la page Accueil">
+        <img src="{!! asset("assets/img/logo_bg_phone.svg") !!}" alt="Logo les pattes heureuses">
+    </a>
+
 </ul>
+
+
