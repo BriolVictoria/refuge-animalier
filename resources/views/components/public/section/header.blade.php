df712517bc0cb4984db33ae0fc56832a9a2d3ecb
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-19 16:51:03 +0100

    Change the title of the link

--- resources/views/components/public/section/header.blade.php
+++ resources/views/components/public/section/header.blade.php
@@ -4,7 +4,7 @@
     {{--Menu (liens)--}}
     <nav class="relative flex justify-between items-center  pl-[1.135rem] pr-[1.135rem] pt-[1rem] pb-[0.625rem]">
         <h2 class="sr-only">Navigation principale</h2>
-        <a href="{!! route('public.homepage') !!}" title="Vers la page d'accueil">
+        <a href="{!! route('public.homepage') !!}" title="Vers la page Accueil">
             <img width="50" height="40" src="{!! asset("assets/img/paws_logo.svg") !!}"
                  alt="Logo représentant une empreinte de patte verte">
         </a>
