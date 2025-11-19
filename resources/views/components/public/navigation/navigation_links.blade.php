f9d8834d97391c7afb706c4423355b1570fe5ec7
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-19 14:22:00 +0100

    Add the burger menu in blade as a component and add CSS + import the changes in the app css

---
+++ resources/views/components/public/navigation/navigation_links.blade.php
@@ -0,0 +1,8 @@
+<ul class="nav_container flex flex-col items-center">
+    <li class="links_bg">Accueil</li>
+    <li class="links_bg">À propos</li>
+    <li class="links_bg">Nos animaux</li>
+    <li class="links_bg">Devenir bénévole</li>
+    <li class="link_bg">Contactez-nous</li>
+    <img src="{!! asset("assets/img/logo_bg.svg") !!}" alt="Logo les patte sheuresue">
+</ul>
