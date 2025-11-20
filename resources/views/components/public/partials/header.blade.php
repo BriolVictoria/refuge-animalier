f72933c4eff274d47b7d81d286854a7558895021
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-20 17:26:36 +0100

    Change a little thing into the header

--- resources/views/components/public/partials/header.blade.php
+++ resources/views/components/public/partials/header.blade.php
@@ -1,8 +1,8 @@
-<header class="bg-white shadow-xl">
+<header class="bg-white shadow-xl px-[1.25rem]">
     <h1 class="sr-only">Les pattes heureuses</h1>
 
     {{--Menu (liens)--}}
-    <nav class="relative flex justify-between items-center  pl-[1.135rem] pr-[1.135rem] pt-[1rem] pb-[0.625rem]">
+    <nav class="relative flex justify-between items-center pt-[1rem] pb-[0.625rem]">
         <h2 class="sr-only">Navigation principale</h2>
         <a href="{!! route('public.homepage') !!}" title="Vers la page Accueil">
             <img width="50" height="40" src="{!! asset("assets/img/paws_logo.svg") !!}"
