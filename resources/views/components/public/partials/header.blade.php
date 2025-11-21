cc6f044e04efc2d967eec55921e9d2c7f0255a22
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-21 12:09:10 +0100

    Add the shadow

--- resources/views/components/public/partials/header.blade.php
+++ resources/views/components/public/partials/header.blade.php
@@ -1,8 +1,8 @@
-<header class="bg-white shadow-xl px-[1.25rem]">
+<header class="relative bg-white shadow-[var(--shadow-xl)] px-[1.25rem] z-10">
     <h1 class="sr-only">Les pattes heureuses</h1>
 
     {{--Menu (liens)--}}
-    <nav class="relative flex justify-between items-center pt-[1rem] pb-[0.625rem]">
+    <nav class="relative flex justify-between items-center pt-4 pb-2.5 ">
         <h2 class="sr-only">Navigation principale</h2>
         <a href="{!! route('public.homepage') !!}" title="Vers la page Accueil">
             <img width="50" height="40" src="{!! asset("assets/img/paws_logo.svg") !!}"
