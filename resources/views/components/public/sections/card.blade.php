c0bbbaa0493e2398ab9996670d1e6377bec8b9e9
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-21 12:08:43 +0100

    Change the foreach, add a span and a figure

--- resources/views/components/public/sections/card.blade.php
+++ resources/views/components/public/sections/card.blade.php
@@ -9,17 +9,40 @@
     'btn_class',
 ])
 
-<article class="bg-white rounded-sm">
+<article class="bg-white rounded-lg relative">
     <h3 class="sr-only">{!! $section_title !!}</h3>
-    <img class="rounded-t-sm rounded-tr-sm max-h-[11.5625rem] object-cover" width="500" height="400" src="{!! $image_path !!}" alt="{!! $image_alt !!}">
+    <figure>
+        <img class="rounded-t-lg rounded-tr-lg max-h-[11.5625rem] object-cover" width="500" height="400"
+             src="{!! $image_path !!}" alt="{!! $image_alt !!}">
+    </figure>
+
+    <span class="absolute bg-blue-100 text-xs font-light px-4.5 py-1 rounded-4xl top-3 right-3">{!! $definitions['statut'] !!}</span>
     <div class="flex flex-col gap-3.5 p-3.5">
         <dl class="grid grid-cols-2 gap-y-4 gap-x-10.5">
-            @foreach($definitions as $definition)
-                <div class="flex">
-                    <dt class="text-xs font-normal pr-2.5">{!! $definition['title'] !!}</dt>
-                    <dd class="text-xs font-bold">{!! $definition['content'] !!}</dd>
-                </div>
-            @endforeach
+            <div class="flex">
+                <dt class="text-xs font-norma pr-2.5">Nom: </dt>
+                <dd class="text-xs font-bold">{!! $definitions['name'] !!}</dd>
+            </div>
+            <div class="flex">
+                <dt class="text-xs font-normal pr-2.5">Age: </dt>
+                <dd class="text-xs font-bold">{!! $definitions['age'] !!}</dd>
+            </div>
+            <div class="flex">
+                <dt class="text-xs font-normal pr-2.5">Race: </dt>
+                <dd class="text-xs font-bold">{!! $definitions['breed'] !!}</dd>
+            </div>
+            <div class="flex">
+                <dt class="text-xs font-normal pr-2.5">Pelage: </dt>
+                <dd class="text-xs font-bold">{!! $definitions['color'] !!}</dd>
+            </div>
+            <div class="flex">
+                <dt class="text-xs font-normal pr-2.5">Date: </dt>
+                <dd class="text-xs font-bold">{!! $definitions['date'] !!}</dd>
+            </div>
+            <div class="flex">
+                <dt class="text-xs font-normal pr-2.5">Attitude: </dt>
+                <dd class="text-xs font-bold">{!! $definitions['attitude'] !!}</dd>
+            </div>
         </dl>
 
         <x-public.buttons.button
