3f89afa5b6bb5a9bb1e59108089486f6d76a71b2
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-21 13:57:28 +0100

    Add the correct route

--- resources/views/components/public/navigation/navigation_link.blade.php
+++ resources/views/components/public/navigation/navigation_link.blade.php
@@ -2,8 +2,8 @@
     'list_class',
     'links_class',
     'label',
-    /*'route_name',*/
+    'route_name',
     'title'
 ])
 
-<li class="{!! $list_class !!}"><a class="{!! $links_class !!}" href="#{{--{!! $route_name !!}--}}" title="{!! $title !!}">{!! $label !!}</a></li>
+<li class="{!! $list_class !!}"><a class="{!! $links_class !!}" href="{!! $route_name !!}" title="{!! $title !!}">{!! $label !!}</a></li>
