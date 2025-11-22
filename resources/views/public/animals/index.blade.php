ef83e04e67fedcf092aa3c25b7dd61c5c6ec5457
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-22 15:26:21 +0100

    Add the index page, add the components, the database animals

---
+++ resources/views/public/animals/index.blade.php
@@ -0,0 +1,36 @@
+@php
+    $buttons =[
+       ['route_name'=>'#', 'title'=> 'Rechercher', 'label' => 'Rechercher', 'class' => 'border-blue-900 border-[0.09375rem] text-blue-900'],
+       ['route_name'=>'#', 'title'=> 'Filtrer', 'label' => 'Filtrer', 'class' => 'border-blue-900 border-[0.09375rem] text-blue-900'],
+    ];
+@endphp
+
+<x-public.app title="Nos animaux">
+
+    <main>
+        <x-public.sections.text-media
+            class="bg-green-50"
+            :image_path="asset('assets/img/animalspage_picture.png')"
+            image_alt="Photo d’un chien qui souris beaucoup placé au centre d’un cercle vert"
+            title="Nos animaux disponible"
+            content="Chaque jour, de nouveaux compagnons attendent un foyer aimant.Découvrez nos pensionnaires en attente d’adoption et laissez-vous toucher par leur histoire."
+            :have_button="true"
+            btn_url="#"
+            btn_title="Vers la page Adopté"
+            btn_label="Adopté maintenant&nbsp;!"
+            btn_class="bg-blue-900 text-white"
+        />
+
+        <x-public.sections.section-animals-card
+        title="Nos animaux"
+        :buttons="$buttons"
+        :animals="$animals"
+        />
+
+    </main>
+
+</x-public.app>
+
+{{--@foreach($animals as $animal)
+    <p>{{$animal->name}}{{$animal->state}}</p>
+@endforeach--}}
