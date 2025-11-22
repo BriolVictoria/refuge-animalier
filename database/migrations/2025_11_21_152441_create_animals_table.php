a4220512166111251d9719c7137948b050be5fee
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-22 11:24:05 +0100

    Change a space

--- database/migrations/2025_11_21_152441_create_animals_table.php
+++ database/migrations/2025_11_21_152441_create_animals_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->timestamp('date');
             $table->string('attitude');
             $table->string('state');
-
             $table->timestamps();
         });
     }
