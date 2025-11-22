cf5f7d90c695465754cc48771fc2bf39247b7ef9
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-22 15:26:40 +0100

    Add a new components including the animals create in DB

---
+++ resources/views/components/public/sections/section-animals-card.blade.php
@@ -0,0 +1,47 @@
+@props([
+    'title',
+    'buttons',
+    'animals',
+])
+
+<section class="flex flex-col items-center gap-6 md:flex-row-reverse">
+    <div class="flex flex-col gap-12">
+        <h2 class="text-center text-xl font-medium">{!! $title !!}</h2>
+        {{--Ajouter les boutons de recherche et de filtre--}}
+        {{--Regarder pour la taille des boutons--}}
+
+        <div class="flex flex-col gap-4 md:flex-row">
+            @foreach($buttons as $button)
+                <x-public.buttons.button
+                    :route_name="$button['route_name']"
+                    :title="$button['title']"
+                    :label="$button['label']"
+                    :class="$button['class']"/>
+            @endforeach
+        </div>
+
+            @foreach($animals as $animal)
+                <x-public.sections.card
+                    :section_title="'Animal:'. $animal->name"
+                    :image_path="asset('assets/img/image_animal.png')"
+                    image_alt="Image d'un chien (un golden) couché sur de l'herbe"
+                    :definitions="[
+                        'name' => $animal->name,
+                        'age' => $animal->age,
+                        'breed' => $animal->breed,
+                        'color' => $animal->coat,
+                        'date' => $animal->date,
+                        'attitude' => $animal->attitude,
+                        'statut' => $animal->state,
+                    ]"
+                    btn_url="#"
+                    btn_title="Vers la fiche Pedro"
+                    btn_label="Voir la fiche"
+                    btn_class="border-blue-900 border-[0.09375rem] text-blue-900"
+                />
+            @endforeach
+
+    </div>
+
+
+</section>
