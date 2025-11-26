fa9f5301ff8571c4090dc2ce5b7a5d4d59cb51de
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-26 17:02:13 +0100

    Modify the input component

--- resources/views/components/public/form/fields/input.blade.php
+++ resources/views/components/public/form/fields/input.blade.php
@@ -6,8 +6,8 @@
     'placeholder',
 ])
 
-<div {!! $attributes->merge(['class'=>'flex flex-col gap-1']) !!}>
-    <label class="text-sm font-normal" for="{!! $field_name !!}">{!! $label !!}</label>
+<div {!! $attributes->merge(['class'=>'flex flex-col gap-1 ']) !!}>
+    <label class="text-sm font-normal" for="{!! $field_name !!}">{!! $label !!}  <span class="text-xs font-light text-red-500">*</span></label>
     <input class="text-sm font-normal text-blue-400 border border-blue-400 px-3 py-3 rounded-sm" type="{!! $type !!}"
            name="{!! $field_name !!}" id="{!! $field_name !!}" value="{!! $value ??  old($field_name) !!}"
            placeholder="{!! $placeholder ?? '' !!}">
