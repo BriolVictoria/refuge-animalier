ce6d6462978a5a184f5999e64553cd5f71cd7a52
BriolVictoria <victoria.briol@student.hepl.be>
2025-11-26 10:21:34 +0100

    Modify my component for the contact form

--- resources/views/public/contactpage.blade.php
+++ resources/views/public/contactpage.blade.php
@@ -8,17 +8,66 @@
 
 @endphp
 
-<x-public.app title="Page de contact">
+<x-public.app title="Formulaire de contact">
 
     <x-public.sections.section-contact-forms
         title="Vous avez une question sur le refuge &nbsp;?"
         content="Ecrivez-nous un message pour qu’on puisse y répondre"
         sub_title="Nos coordonnées"
         :coords="$coords"
-        btn_url="#"
-        btn_title="Envoyer le message"
-        btn_label="Envoyer le message"
-        btn_class="bg-blue-900 text-white self-start"
     />
+
+    <x-public.sections.form>
+        <x-slot:title>
+            Formulaire de contact
+        </x-slot:title>
+        <x-slot:sub_title>
+            Les champs * sont des champs requis
+        </x-slot:sub_title>
+        <x-slot:content>
+            <x-public.form.fields.input
+                field_name="name"
+                label="Nom"
+                type="text"
+                placeholder="Smith"
+            />
+
+            <x-public.form.fields.input
+                field_name="first-name"
+                label="Prénom"
+                type="text"
+                placeholder="Ambre"
+            />
+
+            <x-public.form.fields.input
+                field_name="email"
+                label="Adresse mail"
+                type="email"
+                placeholder="ambre.smith@gmail.com"
+            />
+
+            <x-public.form.fields.input
+                field_name="object"
+                label="Objet"
+                type="text"
+                placeholder="Votre objet"
+            />
+
+            <x-public.form.fields.textarea
+                field_name="message"
+                label="Message"
+                placeholder="Votre message"
+            />
+
+
+            <x-public.buttons.button
+                route_name="#"
+                title="Envoyer le message"
+                label="Envoyer le message"
+                class="bg-blue-900 text-white self-start"/>
+        </x-slot:content>
+    </x-public.sections.form>
+
+
 
 </x-public.app>
