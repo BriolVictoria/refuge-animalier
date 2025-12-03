0f00ff17e7fe0eaf8b2da8a7d9bbaee481739aa0
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-03 09:32:15 +0100

    Add the responsive on the form

--- resources/views/components/public/sections/form.blade.php
+++ resources/views/components/public/sections/form.blade.php
@@ -5,10 +5,14 @@
 ])
 
 
-<section {!! $attributes->merge(['class' => 'flex flex-col gap-1 gap-6']) !!} >
-<div class="flex flex-col gap-3">
+<section {!! $attributes->merge(['class' => 'flex flex-col gap-1 gap-6 lg:px-0']) !!} >
+<div class="flex flex-col gap-3 ">
     <h2 class="text-lg font-medium">{!! $title !!}</h2>
     <p class="text-xs font-light text-red-500">{!! $sub_title !!}</p>
 </div>
-    {!! $content !!}
+    <form class="flex flex-col gap-6"
+        method="POST" action="">
+        {!! $slot !!}
+    </form>
+
 </section>
