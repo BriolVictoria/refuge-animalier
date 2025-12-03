1226e66d24bbe8a20faa49839d17979478a83a5a
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-03 09:33:10 +0100

    Add the responsive on the homepage-hero

--- resources/views/components/public/sections/homepage-hero.blade.php
+++ resources/views/components/public/sections/homepage-hero.blade.php
@@ -9,17 +9,17 @@
     'buttons',
 ])
 
-<section class="bg-green-50 flex flex-col items-center gap-6 md:flex-row-reverse">
-    <figure>
-        <img src="{!! $image_path !!}"
+<section class="bg-green-50 flex flex-col items-center gap-6 2xl:grid 2xl:grid-cols-12 min-[1850px]:justify-between">
+    <figure class="2xl:col-start-8 2xl:col-end-13 2xl:flex 2xl:justify-center">
+        <img class="2xl:w-[34rem]" src="{!! $image_path !!}"
              alt="{!! $image_alt !!}">
     </figure>
-    <div>
-        <span class="text-sm font-normal">{!! $sub_title !!}</span>
-        <h2 class="text-xl font-normal text-green-900 font-[PatrickHand]">
+    <div class="flex flex-col 2xl:col-start-1 2xl:col-end-7 2xl:row-start-1">
+        <span class="text-sm font-normal 2xl:text-xl">{!! $sub_title !!}</span>
+        <h2 class="text-xl font-normal text-green-900 font-[PatrickHand] md:text-2xl 2xl:text-6xl">
             {!! $title !!}
         </h2>
-        <p class="text-sm font-light pt-3 pb-12 ">
+        <p class="text-sm font-light pt-3 pb-12 md:text-lg">
             {!! $content !!}
         </p>
 
