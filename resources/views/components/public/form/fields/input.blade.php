6bd5d5e0230187b1c752c66544de5c02b0be6695
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-03 09:33:28 +0100

    Add the responsive on the input

--- resources/views/components/public/form/fields/input.blade.php
+++ resources/views/components/public/form/fields/input.blade.php
@@ -6,9 +6,9 @@
     'placeholder',
 ])
 
-<div {!! $attributes->merge(['class'=>'flex flex-col gap-1 ']) !!}>
+<div {!! $attributes->merge(['class'=>'flex flex-col gap-1']) !!}>
     <label class="text-sm font-normal" for="{!! $field_name !!}">{!! $label !!}  <span class="text-xs font-light text-red-500">*</span></label>
-    <input class="text-sm font-normal text-blue-400 border border-blue-400 px-3 py-3 rounded-sm" type="{!! $type !!}"
+    <input class="text-sm font-normal text-blue-900 border border-blue-400 px-3 py-3 rounded-sm" type="{!! $type !!}"
            name="{!! $field_name !!}" id="{!! $field_name !!}" value="{!! $value ??  old($field_name) !!}"
            placeholder="{!! $placeholder ?? '' !!}">
 </div>
