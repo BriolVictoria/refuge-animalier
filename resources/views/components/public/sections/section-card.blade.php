0e4feb20cb25090cd762418215b84ac1e63c07f7
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-03 09:35:26 +0100

    Add the responsive on the section card

--- resources/views/components/public/sections/section-card.blade.php
+++ resources/views/components/public/sections/section-card.blade.php
@@ -20,8 +20,8 @@
     'btn_class',
 ])
 
-<section class="bg-green-50 flex flex-col items-center gap-6 md:flex-row-reverse">
-    <div class="flex flex-col gap-4">
+<section class="bg-green-50 flex flex-col items-center gap-8 md:grid md:grid-cols-9 2xl:grid-cols-12">
+    <div class="flex flex-col gap-4 md:col-start-3 md:col-end-8 2xl:col-start-4 2xl:col-end-10">
         <h2 class="text-center text-xl font-medium">
             {!! $title!!}
         </h2>
@@ -29,13 +29,15 @@
             {!! $content!!}
         </p>
     </div>
-    @foreach($animals as $animal)
-
-        <x-public.sections.card
-            :section_title="'Animal:'. $animal->name"
-            :image_path="asset('assets/img/image_animal.png')"
-            image_alt="Image d'un chien (un golden) couché sur de l'herbe"
-            :definitions="[
+    <div
+        class="flex flex-col gap-8 items-center md:col-start-3 md:col-end-8 lg:flex-row lg:col-start-1 lg:justify-center lg:col-end-10 2xl:gap-8 2xl:flex-row 2xl:col-start-2 2xl:col-end-12">
+        @foreach($animals as $animal)
+            <x-public.sections.card
+                :section_title="'Animal:'. $animal->name"
+                :image_path="asset('assets/img/image_animal.png')"
+                image_alt="Image d'un chien (un golden) couché sur de l'herbe"
+                :animal="$animal"
+                :definitions="[
                         'name' => $animal->name,
                         'age' => $animal->age,
                         'breed' => $animal->breed,
@@ -43,19 +45,18 @@
                         'attitude' => $animal->attitude,
                         'statut' => $animal->state,
                     ]"
-            btn_url="{!! route('public.animals.show', $animal->id) !!}"
-            btn_title="Vers la fiche Pedro"
-            btn_label="Voir la fiche"
-            btn_class="border-blue-900 border-[0.09375rem] text-blue-900"
-        />
-
-    @endforeach
+                btn_url="{!! route('public.animals.show', $animal->id) !!}"
+                :btn_title="'Vers la fiche '. $animal->name"
+                btn_label="Voir la fiche"
+                btn_class="border-blue-900 border-[0.09375rem] text-blue-900"
+            />
+        @endforeach
+    </div>
 
     <x-public.buttons.button
         :route_name="$btn_url"
         :title="$btn_title"
         :label="$btn_label"
         :class="$btn_class"/>
-
 
 </section>
