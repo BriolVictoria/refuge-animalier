c58e5d6bf33547eef7cef752e5f7fe156b390b79
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-03 09:38:21 +0100

    Add the responsive on the volunteerpage

--- resources/views/public/volunteerpage.blade.php
+++ resources/views/public/volunteerpage.blade.php
@@ -9,72 +9,71 @@
 @endphp
 
 <x-public.app title="Formulaire de demande de bénévolat">
+    <main class="lg:flex lg:flex-row ">
 
     <x-public.sections.section-contact-forms
-        title="Envie de nous aider &nbsp;?"
-        content="Faites votre demande d’adoption pour adopter votre animal de rêve"
-        sub_title="Nos coordonnées"
-        :coords="$coords"
-    />
+            title="Envie de nous aider &nbsp;?"
+            content="Faites votre demande d’adoption pour adopter votre animal de rêve"
+            sub_title="Nos coordonnées"
+            :coords="$coords"
+        />
 
-    <x-public.sections.form>
-        <x-slot:title>
-            Formulaire de demande de bénévolat
-        </x-slot:title>
-        <x-slot:sub_title>
-            Les champs * sont des champs requis
-        </x-slot:sub_title>
-        <x-slot:content>
-            <x-public.form.fields.input
-                field_name="name"
-                label="Nom"
-                type="text"
-                placeholder="Smith"
-            />
+        <x-public.sections.form
+            class="lg:w-2/3"
+            title="Formulaire de demande de bénévolat"
+            sub_title="Les champs * sont des champs requis">
+            <fieldset class="flex flex-col gap-6">
+                <x-public.form.fields.input
+                    field_name="name"
+                    label="Nom"
+                    type="text"
+                    placeholder="Smith"
+                />
 
-            <x-public.form.fields.input
-                field_name="first-name"
-                label="Prénom"
-                type="text"
-                placeholder="Ambre"
-            />
+                <x-public.form.fields.input
+                    field_name="first-name"
+                    label="Prénom"
+                    type="text"
+                    placeholder="Ambre"
+                />
 
-            <x-public.form.fields.input
-                field_name="email"
-                label="Adresse mail"
-                type="email"
-                placeholder="ambre.smith@gmail.com"
-            />
+                <x-public.form.fields.input
+                    field_name="email"
+                    label="Adresse mail"
+                    type="email"
+                    placeholder="ambre.smith@gmail.com"
+                />
 
-            <x-public.form.fields.input
-                field_name="phone"
-                label="Numéro de téléphone"
-                type="tel"
-                placeholder="+32 4 56 12 76 32 45"
-            />
+                <x-public.form.fields.input
+                    field_name="phone"
+                    label="Numéro de téléphone"
+                    type="tel"
+                    placeholder="+32 4 56 12 76 32 45"
+                />
 
-            <x-public.form.fields.input
-                field_name="address"
-                label="Adresse compléte"
-                type="text"
-                placeholder="Rue des Lilas, Argentine, 3234 n°132"
-            />
+                <x-public.form.fields.input
+                    field_name="address"
+                    label="Adresse compléte"
+                    type="text"
+                    placeholder="Rue des Lilas, Argentine, 3234 n°132"
+                />
 
-            <x-public.form.fields.textarea
-                field_name="message"
-                label="Pourquoi devenir bénévole &nbsp;?"
-                placeholder="Votre message"
-            />
+                <x-public.form.fields.textarea
+                    field_name="message"
+                    label="Pourquoi devenir bénévole &nbsp;?"
+                    placeholder="Votre message"
+                />
 
+            </fieldset>
 
-            <x-public.buttons.button
-                route_name="#"
-                title="Envoyer la demande"
+            <x-public.form.fields.input_submit
                 label="Envoyer la demande"
-                class="bg-blue-900 text-white self-start"/>
-        </x-slot:content>
-    </x-public.sections.form>
+            />
 
+        </x-public.sections.form>
+
+
+    </main>
 
 
 </x-public.app>
