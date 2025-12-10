4ed7d7b2a21b6199f6626a469fba4efb31e72d63
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-10 09:00:53 +0100

    Add the thead element

---
+++ resources/views/components/admin/animals/index/thead.blade.php
@@ -0,0 +1,10 @@
+<thead class="none 2xl:bg-blue-500">
+<tr class="text-white font-medium text-xs">
+    <th class="px-4 py-4">Photo</th>
+    <th class="px-4 py-4">Nom</th>
+    <th class="px-4 py-4">Type</th>
+    <th class="px-4 py-4">Statut</th>
+    <th class="px-4 py-4">Mise à jour</th>
+    <th class="px-4 py-4">Action</th>
+</tr>
+</thead>
