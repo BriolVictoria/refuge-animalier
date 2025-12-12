43ae3436bdb231caebfae018db906890acd47da3
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-12 17:23:48 +0100

    Change the password setting

--- resources/views/components/admin/settings/password.blade.php
+++ resources/views/components/admin/settings/password.blade.php
@@ -7,7 +7,7 @@
 ])
 
 <article class="flex flex-col gap-6 bg-white rounded-sm shadow-[var(--shadow-xl)] p-6 h-full">
-    <h3 class="text-sm font-medium">{!! $title !!}</h3>
+    <h3 class="text-lg font-medium">{!! $title !!}</h3>
     <x-admin.form.field.form>
         <fieldset class="flex flex-col gap-6 lg:grid lg:grid-cols-2">
             {!! $slot!!}
