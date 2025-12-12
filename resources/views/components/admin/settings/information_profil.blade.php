05e2c7fbaf4808ff53e18f2344aef80a14c7ca9e
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-12 17:22:31 +0100

    Change the information profil

--- resources/views/components/admin/settings/information_profil.blade.php
+++ resources/views/components/admin/settings/information_profil.blade.php
@@ -4,7 +4,7 @@
 ])
 
 <article class="flex flex-col gap-6 bg-white rounded-sm shadow-[var(--shadow-xl)] py-6 px-6 h-full">
-    <h3 class="text-sm font-medium">{!! $title !!}</h3>
+    <h3 class="text-lg font-medium">{!! $title !!}</h3>
     <p>{!! $content !!}</p>
     <x-admin.form.field.form>
         <fieldset class="flex flex-col gap-6 md:grid md:grid-cols-2 lg:grid-cols-3">
