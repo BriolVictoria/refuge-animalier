1a1145532d8d0d5e76fffe6a55f2b6b717e9eac1
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-19 12:20:35 +0100

    Change other things

--- resources/views/components/admin/definitions/div.blade.php
+++ resources/views/components/admin/definitions/div.blade.php
@@ -1,21 +1,25 @@
 @props([
-    'definitions'
+    'adopting'
 ])
 
 <div>
-    <dl class="bg-blue-200 px-3 py-4 rounded-sm">
-        <div class="flex items-center gap-4">
-            <dt class="text-sm font-medium">Adoptant&nbsp;:</dt>
-            <dd class="text-xs font-light text-center">{!! $definitions['adopting'] !!}</dd>
-        </div>
-        <div class="flex items-center gap-4">
-            <dt class="text-sm font-medium ">Nom de l'animal&nbsp;:</dt>
-            <dd class="text-xs font-light">{!! $definitions['animal_name'] !!}</dd>
-        </div>
-        <div class="flex items-center gap-4">
-            <dt class="text-sm font-medium">Date&nbsp;:</dt>
-            <dd class="text-xs font-light">{!! $definitions['date'] !!}</dd>
-        </div>
-    </dl>
+    <a href="{!! route('adoptings.show', $adopting->id) !!}">
 
+
+        <dl class="bg-blue-200 px-3 py-4 rounded-sm">
+            <div class="flex items-center gap-4">
+                <dt class="text-sm font-medium">Adoptant&nbsp;:</dt>
+                <dd class="text-xs font-light text-center">{!! $adopting->first_name !!}</dd>
+            </div>
+            <div class="flex items-center gap-4">
+                <dt class="text-sm font-medium ">Nom de l'animal&nbsp;:</dt>
+                <dd class="text-xs font-light">{!! $adopting->animal->name . ' (' .$adopting->animal->breed .')' !!}</dd>
+            </div>
+            <div class="flex items-center gap-4">
+                <dt class="text-sm font-medium">Date&nbsp;:</dt>
+                <dd class="text-xs font-light">{!! $adopting->creation_date !!}</dd>
+            </div>
+        </dl>
+
+    </a>
 </div>
