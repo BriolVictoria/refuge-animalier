c4984830f7913c3e5b67043f4dbf55a85b482cae
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-23 18:27:59 +0100

    Add route

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,25 @@ Route::get('/volunteer', function () {
     return view('public.volunteerpage');
 })->name('public.volunteerpage');
 
-Route::livewire('/admin/dashboard', 'pages::dashboard')->name('dashboard');
+Route::livewire('/admin/dashboard', 'pages::dashboard')->name('dashboard')->middleware('auth');
 
-Route::livewire('/admin/animals', 'pages::animals.index')->name('animals.index');
-Route::livewire('/admin/animals/create', 'pages::animals.create')->name('animals.create');
-Route::livewire('/admin/animals/{id}', 'pages::animals.show')->name('animals.show');
-Route::livewire('/admin/animals/{id}/edit', 'pages::animals.edit')->name('animals.edit');
+Route::livewire('/admin/animals', 'pages::animals.index')->name('animals.index')->middleware('auth');
+Route::livewire('/admin/animals/create', 'pages::animals.create')->name('animals.create')->middleware('auth');
+Route::livewire('/admin/animals/{id}', 'pages::animals.show')->name('animals.show')->middleware('auth');
+Route::livewire('/admin/animals/{id}/edit', 'pages::animals.edit')->name('animals.edit')->middleware('auth');
 
-Route::livewire('/admin/adoptings', 'pages::adoptings.index')->name('adoptings.index');
-Route::livewire('/admin/adoptings/create', 'pages::adoptings.create')->name('adoptings.create');
-Route::livewire('/admin/adoptings/{id}/edit', 'pages::adoptings.edit')->name('adoptings.edit');
-Route::livewire('/admin/adoptings/{id}', 'pages::adoptings.show')->name('adoptings.show');
+Route::livewire('/admin/adoptings', 'pages::adoptings.index')->name('adoptings.index')->middleware('auth');
+Route::livewire('/admin/adoptings/create', 'pages::adoptings.create')->name('adoptings.create')->middleware('auth');
+Route::livewire('/admin/adoptings/{id}/edit', 'pages::adoptings.edit')->name('adoptings.edit')->middleware('auth');
+Route::livewire('/admin/adoptings/{id}', 'pages::adoptings.show')->name('adoptings.show')->middleware('auth');
 
-Route::livewire('/admin/volunteers', 'pages::volunteers.index')->name('volunteers.index');
-Route::livewire('/admin/volunteers/create', 'pages::volunteers.create')->name('volunteers.create');
-Route::livewire('/admin/volunteers/{id}/edit', 'pages::volunteers.edit')->name('volunteers.edit');
-Route::livewire('/admin/volunteers/{id}', 'pages::volunteers.show')->name('volunteers.show');
+Route::livewire('/admin/volunteers', 'pages::volunteers.index')->name('volunteers.index')->middleware('auth');
+Route::livewire('/admin/volunteers/create', 'pages::volunteers.create')->name('volunteers.create')->middleware('auth');
+Route::livewire('/admin/volunteers/{id}/edit', 'pages::volunteers.edit')->name('volunteers.edit')->middleware('auth');
+Route::livewire('/admin/volunteers/{id}', 'pages::volunteers.show')->name('volunteers.show')->middleware('auth');
 
-Route::livewire('/admin/messages', 'pages::messages.index')->name('messages.index');
-Route::livewire('/admin/settings', 'pages::settings')->name('settings');
+Route::livewire('/admin/messages', 'pages::messages.index')->name('messages.index')->middleware('auth');
+Route::livewire('/admin/settings', 'pages::settings')->name('settings')->middleware('auth');
 
 Route::get('/animals', [\App\Http\Controllers\AnimalController::class, 'index'])->name('public.animals.index');
 Route::get('/animals/{animal}', [\App\Http\Controllers\AnimalController::class, 'show'])->name('public.animals.show');
