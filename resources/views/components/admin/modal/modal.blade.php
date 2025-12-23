984b47c00de18d71694e1fee8066484f57a2b4d0
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-23 18:30:22 +0100

    Add the modal

--- resources/views/components/admin/modal/modal.blade.php
+++ resources/views/components/admin/modal/modal.blade.php
@@ -1,9 +1,10 @@
 @props([
     'title',
+    'messages',
 ])
 
 <div class="fixed h-full w-full z-50 bg-[rgba(0,0,0,0.4)] inset-0 flex items-center justify-center">
-    <div class="p-10 bg-white flex flex-col gap-6">
+    <div class="p-10 bg-white flex flex-col gap-7 max-w-1/2">
         <div class="flex items-center justify-between">
             <h1 class=" text-lg text-blue-900 font-semibold md:text-lg">{!! $title !!}</h1>
             <svg wire:click="closeModal()" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" stroke="#2B517A"
