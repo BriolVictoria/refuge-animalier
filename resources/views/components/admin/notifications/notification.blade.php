bd4bf884c2e716cea10cecd8b024cbefba6b259c
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-23 18:29:29 +0100

    Add the notifications

--- resources/views/components/admin/notifications/notification.blade.php
+++ resources/views/components/admin/notifications/notification.blade.php
@@ -1,10 +1,6 @@
-@props([
-    'notifications',
-])
-
-@foreach($notifications as $notification)
+@foreach($this->notifications as $notification)
 <div class="flex items-center gap-3 border-b border-b-blue-900 pb-2 mb-6 lg:mb-10">
-        <img src="{!! $notification['route'] !!}" alt="{!! $notification['image_alt'] !!}">
-        <p class="text-xs font-light">{!! $notification['notification_text'] !!}</p>
+        <img src="{!! asset('assets/img/mail_icon.svg') !!}" alt="Icône d'une enveloppe">
+        <p class="text-xs font-light">{!! $notification->text !!}</p>
 </div>
 @endforeach
