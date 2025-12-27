017b7b9f2379102426df6df6dd6b85f861d2fb54
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-27 13:42:38 +0100

    Edit volunteer pages

--- resources/views/components/admin/volunteers/index/tr.blade.php
+++ resources/views/components/admin/volunteers/index/tr.blade.php
@@ -27,27 +27,29 @@
                 </svg>
 
             </a>
-            <a title="Supprimer la fiche de {!! $volunteer->first_name !!}" href="#" class="inline-block pr-2 transition-all duration-300 hover:scale-105">
+            <x-admin.button.delete_button
+                wire_delete="deleteVolunteer({!! $volunteer->id !!})"
+                delete_message="Supprimer la fiche de {!! $volunteer->first_name !!}"
+                class="inline-block pr-2 transition-all duration-300 hover:scale-105"
+            >
                 <svg width="24" height="27" viewBox="0 0 24 27"
                      xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.375 11.875V19.375" stroke="#26486C" stroke-width="1.25"
                           stroke-linecap="round" fill="none" stroke-linejoin="round"/>
-                    <path d="M14.375 11.875V19.375" stroke="#26486C" stroke-width="1.25"
-                          stroke-linecap="round" fill="none" stroke-linejoin="round"/>
+                    <path d="M14.375 11.875V19.375" stroke="#26486C" fill="none" stroke-width="1.25"
+                          stroke-linecap="round" stroke-linejoin="round"/>
                     <path
                         d="M20.625 5.625V23.125C20.625 23.788 20.3616 24.4239 19.8928 24.8928C19.4239 25.3616 18.788 25.625 18.125 25.625H5.625C4.96196 25.625 4.32607 25.3616 3.85723 24.8928C3.38839 24.4239 3.125 23.788 3.125 23.125V5.625"
-                        stroke="#26486C" stroke-width="1.25" fill="none" stroke-linecap="round"
+                        stroke="#26486C" fill="none" stroke-width="1.25" stroke-linecap="round"
                         stroke-linejoin="round"/>
-                    <path d="M0.625 5.625H23.125" stroke="#26486C" fill="none" stroke-width="1.25"
+                    <path d="M0.625 5.625H23.125" fill="none" stroke="#26486C" stroke-width="1.25"
                           stroke-linecap="round" stroke-linejoin="round"/>
                     <path
                         d="M6.875 5.625V3.125C6.875 2.46196 7.13839 1.82607 7.60723 1.35723C8.07607 0.888392 8.71196 0.625 9.375 0.625H14.375C15.038 0.625 15.6739 0.888392 16.1428 1.35723C16.6116 1.82607 16.875 2.46196 16.875 3.125V5.625"
-                        stroke="#26486C" stroke-width="1.25" fill="none" stroke-linecap="round"
+                        stroke="#26486C" fill="none" stroke-width="1.25" stroke-linecap="round"
                         stroke-linejoin="round"/>
                 </svg>
-
-
-            </a>
+            </x-admin.button.delete_button>
 
 
         </td>
--- resources/views/pages/volunteers/⚡index/index.php
+++ resources/views/pages/volunteers/⚡index/index.php
@@ -6,9 +6,22 @@ use Livewire\Component;
 
 new class extends Component
 {
+    public string $term = '';
+
     #[Computed]
     function volunteers()
     {
-        return Volunteer::paginate(10);
+        return Volunteer::query()
+            ->where('first_name', 'like', '%' . $this->term . '%')
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+    }
+
+    public function deleteVolunteer(int $id):void
+    {
+        $volunteer = Volunteer::findOrFail($id);
+        $volunteer->delete();
+
+        $this->redirectRoute('volunteers.index', navigate: true);
     }
 };
