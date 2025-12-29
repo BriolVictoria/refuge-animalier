fa1682e67bb5283c98b4c0bdad4a64cae1843b2c
BriolVictoria <victoria.briol@student.hepl.be>
2025-12-29 16:53:49 +0100

    Edit settings pages

--- resources/views/components/admin/navigation/nav_bar.blade.php
+++ resources/views/components/admin/navigation/nav_bar.blade.php
@@ -3,11 +3,15 @@
 
     <div class="lg:mt-auto">
         <div class="flex py-4 border-t border-t-blue-300 ">
-            <a class="flex transition-all duration-300 hover:scale-101 hover:text-blue-800" href="{!! route('settings') !!}">
-                <img class="max-w-1/4" width="55" height="55" src="{!! asset('assets/img/profil_image.svg') !!}" alt="Image de profil">
+            <a class="flex transition-all duration-300 hover:scale-101 hover:text-blue-800"
+               href="{!! route('settings') !!}">
+                <img
+                    alt="Image de profil"
+                    src="{{ auth()->user()->avatar ? asset('storage/' . auth()->user()->avatar) : asset('assets/img/profil_image.svg') }}"
+                    class="aspect-square w-12 h-12 rounded-full object-cover object-center">
                 <div class="flex flex-col px-3">
-                    <span class="text-lg font-normal">John Doe</span>
-                    <span class="text-xs font-light">john.doe@gmail.com</span>
+                    <span class="text-lg font-normal">{!! auth()->user()->name !!}</span>
+                    <span class="text-xs font-light">{!! auth()->user()->email !!}</span>
                 </div>
             </a>
         </div>
